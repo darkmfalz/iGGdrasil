7b64bc8f968b4b888f36c6c8f42c98f534d47892
Adeeb Sheikh <asheikh4@u.rochester.edu>
2015-10-31 17:37:21 -0400

    Made the cookie authentication code. Updated CSS.

--- server/cgi-bin/server-authenticate.py
+++ server/cgi-bin/server-authenticate.py
@@ -1,40 +1,49 @@
 #!"C:\Python27\python.exe"
 
 import cgitb
+import cgi
+import sqlite3
+import Cookie
+import datetime
+import os
+import json
+
 cgitb.enable()
 
-import cgi
 form = cgi.FieldStorage()
 
-#Retrieve the username and password from the HTML field
-requested_username = form['requested_username'].value
-
-#initialize use of the database
-import sqlite3
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
-
 
 #Comments for this section are just the comments from above
 print "Content-type: text/html"
 
-import json
 data = {}
 
-#Checks over the accounts for the given username -- the username is encoded in hex, to prevent SQL injection
-for r in c.execute('select * from accounts where username=?', [requested_username.encode('hex')]):
+stored_cookie_string = os.environ.get('HTTP_COOKIE')
+if not stored_cookie_string:
+	pass
+else:
+	cookie = Cookie.SimpleCookie(stored_cookie_string)
+	if 'username' in cookie:
+		requested_username = ""
+		for r in c.execute('select * from loggedin where sessionid=?', [old['username'].value]):
+			requested_username = r[1]
 
-	username = r[0].decode('hex')
-	firstname = r[1].decode('hex')
-	lastname = r[2].decode('hex')
-	image = r[3].decode('hex')
+		#Checks over the accounts for the given username -- the username is encoded in hex, to prevent SQL injection
+		for r in c.execute('select * from accounts where username=?', [requested_username.encode('hex')]):
 
-	data['username'] = username
-	data['firstname'] = firstname
-	data['lastname'] = lastname
-	data['image'] = image
+			username = r[0].decode('hex')
+			firstname = r[1].decode('hex')
+			lastname = r[2].decode('hex')
+			image = r[3].decode('hex')
 
-	print
-	print json.dumps(data)
+			data['username'] = username
+			data['firstname'] = firstname
+			data['lastname'] = lastname
+			data['image'] = image
+
+			print
+			print json.dumps(data)
 
 conn.close()
