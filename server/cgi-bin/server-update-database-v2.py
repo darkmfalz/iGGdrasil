d125a4270141a75873054d24095dc13f3c91f5f7
Adeeb Sheikh <asheikh4@u.rochester.edu>
2015-11-21 12:59:41 -0500

    Moved the client-side login/logout into an external JS script.

---
+++ server/cgi-bin/server-update-database-v2.py
@@ -0,0 +1,13 @@
+#!"C:\Python27\python.exe"
+
+import sqlite3
+
+# create a database file named 'people.db' if it doesn't exist yet.
+# if this file already exists, then the program will quit.
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+# create a new 'users' table with three columns: name, age, image
+c.execute('create table comments(id varchar(200) primary key not null, username varchar(200), parentid varchar(200), created date, comment text, grammar boolean)')
+
+conn.close()
--- server/cgi-bin/server-user.py
+++ server/cgi-bin/server-user.py
@@ -67,94 +67,10 @@ def redirect():
 
 			});
 
-			$(document).on('submit', '#login', function(e){
-
-				//get the username and password from the textbox
-				var username = $("#username").val();
-				var password = $("#password").val();
-
-				if (document.getElementById('persist_box').checked) {
-					var checked = "TRUE";
-				} 
-				else {
-					var checked = "FALSE";
-				}
-				console.log(username);
-				console.log(checked);
-
-				$.ajax({
-
-					url: "/cgi-bin/server-login.py",
-
-					data: {
-						requested_username: username,
-						requested_password: password,
-						keep_loggedin: checked
-					},
-
-					type: "POST",
-
-					dataType: "json",
-
-					success: function(data){
-
-						console.log("Successfully logged in as:");
-						console.log(data.username);
-						$("#username").val("");
-						$("#password").val("");
-						window.location.assign("./users/".concat(data.username));
-
-					},
-
-					error: function(){
-
-						alert("There was a problem: invalid username/password combination.");
-						$("#username").val("");
-						$("#password").val("");
-
-					}
-
-
-				});
-
-				e.preventDefault();
-
-				return false;
-
-			});
-
-			$(document).on('submit', '#logout', function(e){
-						
-				$.ajax({
-
-					url: "/cgi-bin/server-logout.py",
-
-					data: {
-					},
-
-					type: "POST",
-
-					datatype: "json",
-
-					success: function(data){
-
-						window.location.assign("../login");
-
-					},
-
-					error: function(){
-
-						console.log("Error logging out.");
-
-					}
-
-				});
-
-				e.preventDefault();
-				return false;
-			});
-
 		});
+		</script>
+
+		<script src="/js-bin/client-login-logout.js" type="text/javascript">
 		</script>
 	</head>
 
@@ -264,93 +180,10 @@ def viewProfile():
 
 						});
 
-						$(document).on('submit', '#login', function(e){
-
-							//get the username and password from the textbox
-							var username = $("#username").val();
-							var password = $("#password").val();
-
-							if (document.getElementById('persist_box').checked) {
-								var checked = "TRUE";
-							} 
-							else {
-								var checked = "FALSE";
-							}
-							console.log(username);
-							console.log(checked);
-
-							$.ajax({
-
-								url: "/cgi-bin/server-login.py",
-
-								data: {
-									requested_username: username,
-									requested_password: password,
-									keep_loggedin: checked
-								},
-
-								type: "POST",
-
-								dataType: "json",
-
-								success: function(data){
-
-									console.log("Successfully logged in as:");
-									console.log(data.username);
-									$("#username").val("");
-									$("#password").val("");
-									window.location.assign("/users/".concat(data.username));
-
-								},
-
-								error: function(){
-
-									alert("There was a problem: invalid username/password combination.");
-									$("#username").val("");
-									$("#password").val("");
-
-								}
-
-
-							});
-
-							e.preventDefault();
-
-							return false;
-
-						});
-
-						$(document).on('submit', '#logout', function(e){
-									
-							$.ajax({
-
-								url: "/cgi-bin/server-logout.py",
-
-								data: {
-								},
-
-								type: "POST",
-
-								datatype: "json",
-
-								success: function(data){
-
-									window.location.assign("../login");
-
-								},
-
-								error: function(){
-
-									console.log("Error logging out.");
-
-								}
-
-							});
-
-							e.preventDefault();
-							return false;
-						});
 					});
+				</script>
+
+				<script src="/js-bin/client-login-logout.js" type="text/javascript">
 				</script>
 			</head>
 				
@@ -520,93 +353,10 @@ else:
 
 						});
 
-						$(document).on('submit', '#login', function(e){
-
-							//get the username and password from the textbox
-							var username = $("#username").val();
-							var password = $("#password").val();
-
-							if (document.getElementById('persist_box').checked) {
-								var checked = "TRUE";
-							} 
-							else {
-								var checked = "FALSE";
-							}
-							console.log(username);
-							console.log(checked);
-
-							$.ajax({
-
-								url: "/cgi-bin/server-login.py",
-
-								data: {
-									requested_username: username,
-									requested_password: password,
-									keep_loggedin: checked
-								},
-
-								type: "POST",
-
-								dataType: "json",
-
-								success: function(data){
-
-									console.log("Successfully logged in as:");
-									console.log(data.username);
-									$("#username").val("");
-									$("#password").val("");
-									window.location.assign("/users/".concat(data.username));
-
-								},
-
-								error: function(){
-
-									alert("There was a problem: invalid username/password combination.");
-									$("#username").val("");
-									$("#password").val("");
-
-								}
-
-
-							});
-
-							e.preventDefault();
-
-							return false;
-
-						});
-
-						$(document).on('submit', '#logout', function(e){
-									
-							$.ajax({
-
-								url: "/cgi-bin/server-logout.py",
-
-								data: {
-								},
-
-								type: "POST",
-
-								datatype: "json",
-
-								success: function(data){
-
-									window.location.assign("../login");
-
-								},
-
-								error: function(){
-
-									console.log("Error logging out.");
-
-								}
-
-							});
-
-							e.preventDefault();
-							return false;
-						});
 					});
+				</script>
+
+				<script src="/js-bin/client-login-logout.js" type="text/javascript">
 				</script>
 			</head>
 				
